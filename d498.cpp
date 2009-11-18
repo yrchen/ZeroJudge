c58793d58c9c577b5a9d9b6c4444c57e7cf6696f
Xaver Y.R. Chen <yrchen@ATCity.org>
2009-11-18 16:16:39 +0800

    add problem d498

---
+++ d498.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int
+main()
+{
+    int n;
+    
+    while(cin >> n)
+    {
+        while(n)
+        {
+            cout << "I don't say swear words!" << endl;
+            n--;
+        }
+    }
+    
+    return 0;
+}
