5633c9cd933eda072558cf163befa73714c8c7a5
unknown <ivor@.(none)>
2009-11-19 19:57:22 +0800

    a001.c add

---
+++ a001.c
@@ -0,0 +1,17 @@
+/**********************************************************************************/
+/*  Problem: a001 "«¢Åo" from Brian Kernighan                                   */
+/*  Language: C++                                                                 */
+/*  Result: AC (4ms, 704KB) on ZeroJudge                                          */
+/*  Author: ivor at 2009-11-10 13:38:13                                           */
+/**********************************************************************************/
+
+#include <stdio.h>  
+int main()  
+{  
+   char s[1000];  
+   while(scanf("%s",s)==1)  
+   {  
+     printf("hello, %s\n",s);  
+   }  
+   return 0;
+}  
