9015a9f06f739dd63d633b32dc1b6dc06a700dd9
chchwy <chchwy@yanting-desktop.ntue.edu.tw>
2009-12-13 22:09:45 +0800

    zerojuge cant inlcude climits, Unreasonable

--- a034.cpp
+++ a034.cpp
@@ -4,28 +4,26 @@
 /*  Result: AC (4ms, 688KB) on ZeroJudge                                          */  
 /*  Author: chchwy at 2009-11-10 18:38:21                                         */  
 /**********************************************************************************/  
-  
-#include<cstdio>
-#include<climits>
+#include<iostream>
 int main(){  
-  
-    int x;  
-    while( scanf("%d",&x)==1 ){  
-	
-  		if(x==0){puts("0"); continue;}
-  
-  		unsigned int mask = INT_MIN;  //INT_MIN = 10000...0
-  
-  		//ignore leading zeros
-  		while( (x&mask) == 0 )
+
+	int x;  
+	while( scanf("%d",&x)==1 ){  
+
+		if(x==0){puts("0"); continue;}
+
+		unsigned int mask = INT_MIN;  //INT_MIN = 10000...0
+
+		//ignore leading zeros
+		while( (x&mask) == 0 )
 			mask=mask>>1;
 
-        while(mask!=0){  
-            char c = (x&mask) ? '1' : '0' ;  
-            putchar(c);  
-            mask=mask>>1;  
-        }  
-        putchar('\n');  
-    }  
-    return 0;  
+		while(mask!=0){  
+			char c = (x&mask) ? '1' : '0' ;  
+			putchar(c);  
+			mask=mask>>1;  
+		}  
+		putchar('\n');  
+	}  
+	return 0;  
 }
