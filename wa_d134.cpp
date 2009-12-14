ca22f625176dc1f97b71b39736d3e688a1052590
chchwy <chchwy@yanting-desktop.ntue.edu.tw>
2009-12-14 12:18:36 +0800

    add d134 but still wa

---
+++ wa_d134.cpp
@@ -0,0 +1,31 @@
+/**********************************************************************************/
+/*  Problem: d134 "Q369: Combinations" from ACM-369                               */
+/*  Language: C++                                                                 */
+/*  Result: WA (line:4) on ZeroJudge                                              */
+/*  Author: chchwy at 2009-11-26 14:47:31                                         */
+/**********************************************************************************/
+
+//Non-recursive edition
+//C(n,r)=n! / (n-r)!r!
+#include<iostream>
+using namespace std;
+
+long C(int n,int r){
+	if(n-r<r) r=n-r; // C(5,3)==C(5,2)
+
+	long double product=1;
+	for(int i=1; i<=r; i++){
+		product = product*(n-r+i)/i;
+	}
+	return (long)product;
+}
+
+int main(){
+	int n,r;
+	while(scanf("%d %d",&n,&r)==2){
+		if(n==0 && r==0) break;
+		printf("%d things taken %d at a time is %d exactly.\n",n,r,C(n,r));
+	}
+	return 0;
+}
+
