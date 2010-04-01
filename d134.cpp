3c172d326f2f7aa72624b60fd2c7ea4c76021a06
chchwy <chchwy@yanting-desktop.ntue.edu.tw>
2010-04-01 17:12:02 +0800

    solve d134 by long double

---
+++ d134.cpp
@@ -0,0 +1,32 @@
+/**********************************************************************************/
+/*  Problem: d134 "Q369: Combinations" from ACM-369                               */
+/*  Language: C++                                                                 */
+/*  Result: WA (line:4) on ZeroJudge                                              */
+/*  Author: chchwy at 2009-11-26 14:47:31                                         */
+/**********************************************************************************/
+//Non-recursive edition
+//C(n,r)=n! / (n-r)!r!
+#include<iostream>
+using namespace std;
+
+const double EPS = 0.00000001;
+
+long double C(int n,int r){
+	if(n-r<r) r=n-r; // C(5,3)==C(5,2)
+
+	long double product=1;
+	for(int i=1; i<=r; i++){
+		product = product*(n-r+i)/i;
+	}
+	return (product + EPS);
+}
+
+int main(){
+	int n,r;
+	while(scanf("%d %d",&n,&r)==2){
+		if(n==0 && r==0) break;
+		printf("%d things taken %d at a time is %.0Lf exactly.\n",n,r,C(n,r));
+	}
+	return 0;
+}
+
