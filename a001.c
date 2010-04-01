b0f401aae61e0c9fc8668118182681cb4543a14c
chchwy <chchwy@yanting-desktop.ntue.edu.tw>
2010-04-01 13:15:53 +0800

    solve c092

--- a001.c
+++ a001.c
@@ -6,9 +6,12 @@
 /**********************************************************************************/  
 
 #include<stdio.h>  
+
 int main(){  
 	char buf[2048];  
 	while( fgets(buf, sizeof(buf), stdin)!=NULL)
 		printf("hello, %s",buf);  
-	return 0;    
+	return 0;   
 }
+
+
